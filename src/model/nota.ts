b4c71ff98cbe48e7691aa656419a56ea3e71a3e0
Roger Eduardo de Couto <rogerecouto@gmail.com>
2017-11-27 13:58:01 -0200

    Correção de bugs

--- src/model/nota.ts
+++ src/model/nota.ts
@@ -1,5 +1,10 @@
 export class Nota{
 
-    constructor(public id?:number, public titulo?:string, public texto?:string, public arquivada?: boolean){}
+    constructor(
+        public id?:number, 
+        public titulo?:string, 
+        public texto?:string, 
+        public arquivada?: boolean
+    ){}
     
 }
